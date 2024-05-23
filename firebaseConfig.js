dbc37aae096e628b680469e783610bd3b59296d9
EkalavyanS <101187280+EkalavyanS@users.noreply.github.com>
2024-05-23 11:27:03 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCR0PetcFpv8OaK_krrUr3bOcLSt6cMryA",
-    authDomain: "wish2go-39788.firebaseapp.com",
-    databaseURL: "https://wish2go-39788-default-rtdb.firebaseio.com",
-    projectId: "wish2go-39788",
-    storageBucket: "wish2go-39788.appspot.com",
-    messagingSenderId: "432475094234",
-    appId: "1:432475094234:web:01aba13fbcecab2255f9e3",
-    measurementId: "G-ZHZ7W06KYC"
+    apiKey: "API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    databaseURL: "DATABASE_URL",
+    projectId: "PROJECT_URL",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "MES_ID"
   }
 
 const app = initializeApp(firebaseConfig)
